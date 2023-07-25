15c0e8a0f291971501ca9ea306fbec726e24fe79
Mario Lazaro <mario-msl@hotmail.com>
2023-07-25 13:11:39 -0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Tabuada: ");
+        int tabuada;
+
+        tabuada = scan.nextInt();
+        System.out.println("Tabuada de: ");
+
+        for (int i = 0; i <= 10 ; i++) {
+            System.out.println(tabuada + " x " + i + " = " + (tabuada*i));
+
+        }
+
+
+
     }
 }
